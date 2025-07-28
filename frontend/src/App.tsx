7dedfdf1485003b4c896c913c31eb97879c05db6
Joaquin <nunezjoako9@gmail.com>
2025-07-28 18:46:22 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Prueba from './prueba'
+
+function App() {
+
+  return (
+    <>
+      <Prueba />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/prueba.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function prueba() {
+  return (
+    <>
+      <Button variant={"outline"}>Click me</Button>
+    </>
+  )
+}
+
+export default prueba
